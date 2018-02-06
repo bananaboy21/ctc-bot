1417cef5785ed14be8f307068235c7665441e02f
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-05 20:54:49 -0500

    gettin' em ready

--- bot.py
+++ bot.py
@@ -14,4 +14,30 @@ import textwrap
 from contextlib import redirect_stdout
 from discord.ext import commands
 import json
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'),description="Specialized bot made for HWCWL.\n\nHelp Commands",owner_id=277981712989028353)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'),description="Specialized bot made for COC Talk Club.\n\nHelp Commands",owner_id=277981712989028353)
+
+
+@bot.event
+async def on_ready():
+    print('Bot is online, and ready to ROLL!')
+	await bot.change_presence(game=discord.Game(name="$help"))
+
+
+@bot.command()
+async def twitter(ctx):
+	await ctx.send("Check out Coc Talk Club (@CTC_coc): https://twitter.com/CTC_coc?s=09")
+
+
+@bot.command()
+async def website(ctx):
+	await ctx.send("https://coctalkclub.weebly.com/")
+
+
+@bot.command()
+async def logo(ctx):
+	await ctx.send("The first logo was made by Abuxine Gaming and Jitux Made 3 logos for CTC and Nick clashes made 1 one logo for CTC")
+
+
+if not os.environ.get('TOKEN'):
+    print("no token found REEEE!")
+bot.run(os.environ.get('TOKEN').strip('"'))
