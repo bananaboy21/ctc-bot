8abf663cbae7dac9743cbd19daba9645f7932480
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-06 16:16:30 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,44 +25,38 @@ async def on_ready():
 
 @bot.command()
 async def help(ctx):
-	color = discord.Color(value=0x00ff00)
-	em = discord.Embed(color=color, title='COC Talk Club Bot')
-	em.add_field(name="twitter", value="Returns the CTC's Twitter page.")
-	em.add_field(name="website", value="Returns the CTC's official website.")
-	em.add_field(name="logo", value="Returns facts for the CTC's logo!")
-	em.add_field(name="leagues", value="Returns invite links to some COC leagues!")
-	em.set_thumbnail(url="https://media.discordapp.net/attachments/410033646309736448/410205224213413889/JPEG_20180126_122140.jpg")
-	await ctx.send(embed=em)
+    color = discord.Color(value=0x00ff00)
+    em = discord.Embed(color=color, title='COC Talk Club Bot')
+    em.add_field(name="twitter", value="Returns the CTC's Twitter page.")
+    em.add_field(name="website", value="Returns the CTC's official website.")
+    em.add_field(name="logo", value="Returns facts for the CTC's logo!")
+    em.add_field(name="leagues", value="Returns invite links to some COC leagues!")
+    em.set_thumbnail(url="https://media.discordapp.net/attachments/410033646309736448/410205224213413889/JPEG_20180126_122140.jpg")
+    await ctx.send(embed=em)
 
 
 
 @bot.command()
 async def twitter(ctx):
-	await ctx.send("Check out Coc Talk Club (@CTC_coc): https://twitter.com/CTC_coc?s=09")
+    await ctx.send("Check out Coc Talk Club (@CTC_coc): https://twitter.com/CTC_coc?s=09")
 
 
 @bot.command()
 async def website(ctx):
-	await ctx.send("https://coctalkclub.weebly.com/")
+    await ctx.send("https://coctalkclub.weebly.com/")
 
 
 @bot.command()
 async def logo(ctx):
-	await ctx.send("The first logo was made by Abuxine Gaming and Jitux Made 3 logos for CTC and Nick clashes made 1 one logo for CTC")
+    await ctx.send("The first logo was made by Abuxine Gaming and Jitux Made 3 logos for CTC and Nick clashes made 1 one logo for CTC")
 
 
 @bot.command()
 async def leagues(ctx):
-	color = discord.Color(value=0x00ff00)
-	em = discord.Embed(color=color, title='COC Leagues')
-	em.description = 'Here are some leagues to join!'
-	em.add_field(name="WCL", value="https://discord.gg/Hwk9SbW")
-	em.add_field(name="ICWL", value="https://discord.gg/fNTRU32")
-	em.add_field(name="NCWL", value="https://discord.me/ncwl")
-	em.add_field(name="Gauntlet", value="https://discord.gg/2XtH5nE")
-	em.add_field(name="AWL", value="https://discord.gg/WbK39GJ")
-	em.add_field(name="Clash Champs", value="https://discord.gg/a95eHnq")
-	await ctx.send(embed=em)
+    color = discord.Color(value=0x00ff00)
+    em = discord.Embed(color=color, title='COC Leagues')
+    em.description = 'Here are some leagues to join! \n\nWCL: https://discord.gg/Hwk9SbW \nBWC: https://discord.gg/W5EgkRH \nICWL: https://discord.gg/fNTRU32 \nNCWL: discord.me/ncwl \nGauntlet: https://discord.gg/WbK39GJ \nAWL: https://discord.gg/WbK39GJ \nClash Champs: https://discord.gg/a95eHnq'
+    await ctx.send(embed=em)
 
 
 if not os.environ.get('TOKEN'):
