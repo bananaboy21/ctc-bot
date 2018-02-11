e77efe057d85c17b172aa66a1e07806a31172a74
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-10 20:56:01 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,6 +34,7 @@ async def help(ctx):
     em.add_field(name="tourney", value="Returns invite links to some COC tournaments!")
     em.add_field(name="communities", value="Returns invite links to some COC communities!")
     em.add_field(name="bots", value="Returns invite links to some Discord bot servers!")
+    em.add_field(name="gfx", value="Returns Discord usernames of some amazing COC GFX designers!")
     em.set_thumbnail(url="https://media.discordapp.net/attachments/410033646309736448/410205224213413889/JPEG_20180126_122140.jpg")
     await ctx.send(embed=em)
 
@@ -58,13 +59,13 @@ async def logo(ctx):
 async def leagues(ctx):
     color = discord.Color(value=0x00ff00)
     em = discord.Embed(color=color, title='COC Leagues')
-    em.description = 'Here are some leagues to join! \n\nWCL: https://discord.gg/Hwk9SbW \nBWC: https://discord.gg/W5EgkRH \nICWL: https://discord.gg/fNTRU32 \nNCWL: discord.me/ncwl \nGauntlet: https://discord.gg/WbK39GJ \nAWL: https://discord.gg/WbK39GJ'
+    em.description = 'Here are some leagues to join! \n\nWCL: https://discord.gg/Hwk9SbW \nBWC: https://discord.gg/W5EgkRH \nICWL: https://discord.gg/fNTRU32 \nNCWL: discord.me/ncwl \nGauntlet: https://discord.gg/2XtH5nE \nAWL: https://discord.gg/WbK39GJ'
     await ctx.send(embed=em)
 
 
 @bot.command()
 async def tourney(ctx):
-    color = discord.Color(value=0x00ff00)
+	color = discord.Color(value=0x00ff00)
     em = discord.Embed(color=color, title='COC Tournaments')
     em.description = 'Here are some COC tourneys to join! \n\nClash Champs: https://discord.gg/a95eHnq \nCPE: https://discord.gg/XAEx6Hf \nForums Cup: https://discord.gg/8RNXdXR'
     await ctx.send(embed=em)
@@ -72,18 +73,27 @@ async def tourney(ctx):
 
 @bot.command()
 async def communities(ctx):
-    color = discord.Color(value=0x00ff00)
+	color = discord.Color(value=0x00ff00)
     em = discord.Embed(color=color, title='COC Communities')
-    em.description = "Here are some COC communities to join! \n\nIndian Clash Community: https://discord.gg/qg8wPtv \nCasual Clash: https://discord.io/clash \nClash Tutor:https://discord.gg/SYDB7K9\nSouth African Clash Community:https://discord.gg/HVt929x\nBase Building:https://discord.gg/HR8zT4M\nClash Roulette:https://discord.me/roulette\nPotluck:https://discord.me/cocpotluck\nForums vs Reddit:https://discord.gg/JE86z7c\nCoc Bromance:https://discord.gg/KWjve7c\nGoblins Realm:https://discord.gg/qgSrda2\nZillas Fam:https://discord.gg/bJbj65x\nClash Connections:https://discord.gg/CN9VW3B\nClash Art community:https://discord.gg/BWcbUH7"
+    em.description = "Here are some COC communities to join! \n\nIndian Clash Community: https://discord.gg/qg8wPtv \nCasual Clash: https://discord.io/clash \nClash Tutor: https://discord.gg/SYDB7K9\nSouth African Clash Community: https://discord.gg/HVt929x\nBase Building: https://discord.gg/HR8zT4M\nClash Roulette: https://discord.me/roulette\nPotluck: https://discord.me/cocpotluck\nForums vs Reddit: https://discord.gg/JE86z7c\nCoc Bromance: https://discord.gg/KWjve7c\nGoblins Realm: https://discord.gg/qgSrda2\nZillas Fam: https://discord.gg/bJbj65x\nClash Connections: https://discord.gg/CN9VW3B\nClash Art community: https://discord.gg/BWcbUH7"
     await ctx.send(embed=em)
 
 
 @bot.command()
 async def bots(ctx):
-    color = discord.Color(value=0x00ff00)
+	color = discord.Color(value=0x00ff00)
     em = discord.Embed(color=color, title='Discord Bots')
     em.description = "Here are the servers for some Discord bots. \n\ndat banana bot: https://discord.gg/wvkVknA\nMyBot: https://discord.gg/4ZeNS74\nWmbot: https://discord.me/warmatch\nSidekick-https://discord.gg/ENYgjk"
     await ctx.send(embed=em)
+
+
+@bot.command()
+async def gfx(ctx):
+	color = discord.Color(value=0x00ff00)
+    em = discord.Embed(color=color, title='Clash of Clans GFX Designers')
+    em.description = "Jitux3xD#6545 \nNick clashes#8401  \nBangBangAure#3966 \nBobby_digital72#5621 \nMiss.Behavin#6462 \nfiRe loRd#2593  \nBox, Clash & Art!#8519 \nMini Me#2525"
+    await ctx.send(embed=em)
+
 
 
 if not os.environ.get('TOKEN'):
